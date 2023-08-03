36a7a9aa8a1ff9616632c6af983b79919636c223
LauYann <metalyanni@gmail.com>
2023-08-03 11:42:28 -0600

    Initial Commit

---
+++ PiedraPapelTijera/PiedraPapelTijeraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiedraPapelTijeraApp.swift
+//  PiedraPapelTijera
+//
+//  Created by MacBook 26 on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PiedraPapelTijeraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
